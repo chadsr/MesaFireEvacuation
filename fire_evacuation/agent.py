02405d44bee70be1ff0002eefecb512005cd80fc
Ross Chadwick <ross@rchadwick.co.uk>
2018-01-06 15:12:00 +0100

    comments for bresenham's

--- fire_evacuation/agent.py
+++ fire_evacuation/agent.py
@@ -7,63 +7,61 @@ from enum import Enum
 from mesa import Agent
 
 
-# Credits to http://www.roguebasin.com/index.php?title=Bresenham%27s_Line_Algorithm
 def get_line(start, end):
-    """Bresenham's Line Algorithm
-    Produces a list of tuples from start and end
-
-    >>> points1 = get_line((0, 0), (3, 4))
-    >>> points2 = get_line((3, 4), (0, 0))
-    >>> assert(set(points1) == set(points2))
-    >>> print points1
-    [(0, 0), (1, 1), (1, 2), (2, 3), (3, 4)]
-    >>> print points2
-    [(3, 4), (2, 3), (1, 2), (1, 1), (0, 0)]
     """
-    # Setup initial conditions
+    Implementaiton of Bresenham's Line Algorithm
+    Returns a list of tuple coordinates from starting tuple to end tuple (and including them)
+    """
+    # Break down start and end tuples
     x1, y1 = start
     x2, y2 = end
-    dx = x2 - x1
-    dy = y2 - y1
 
-    # Determine how steep the line is
-    is_steep = abs(dy) > abs(dx)
+    # Calculate differences
+    diff_x = x2 - x1
+    diff_y = y2 - y1
 
-    # Rotate line
-    if is_steep:
+    # Check if the line is steep
+    line_is_steep = abs(diff_y) > abs(diff_x)
+
+    # If the line is steep, rotate it
+    if line_is_steep:
+        # Swap x and y values for each pair
         x1, y1 = y1, x1
         x2, y2 = y2, x2
 
-    # Swap start and end points if necessary and store swap state
+    # If the start point is further along the x-axis than the end point, swap start and end
     swapped = False
     if x1 > x2:
         x1, x2 = x2, x1
         y1, y2 = y2, y1
         swapped = True
 
-    # Recalculate differentials
-    dx = x2 - x1
-    dy = y2 - y1
+    # Calculate the differences again
+    diff_x = x2 - x1
+    diff_y = y2 - y1
 
-    # Calculate error
-    error = int(dx / 2.0)
-    ystep = 1 if y1 < y2 else -1
+    # Calculate the error margin
+    error_margin = int(diff_x / 2.0)
+    step_y = 1 if y1 < y2 else -1
 
-    # Iterate over bounding box generating points between start and end
+    # Iterate over the bounding box, generating coordinates between the start and end coordinates
     y = y1
-    points = []
-    for x in range(x1, x2 + 1):
-        coord = (y, x) if is_steep else (x, y)
-        points.append(coord)
-        error -= abs(dy)
-        if error < 0:
-            y += ystep
-            error += dx
+    path = []
 
-    # Reverse the list if the coordinates were swapped
+    for x in range(x1, x2 + 1):
+        coord = (y, x) if line_is_steep else (x, y)  # Get a coordinate according to if x and y values were swapped
+        path.append(coord)  # Add it to our path
+        error_margin -= abs(diff_y)  # Deduct the absolute difference of y values from our error_margin
+
+        if error_margin < 0:  # When the error margin drops below zero, increase y by the step and the error_margin by the x difference
+            y += step_y
+            error_margin += diff_x
+
+    # The the start and end were swapped, reverse the path
     if swapped:
-        points.reverse()
-    return points
+        path.reverse()
+
+    return path
 
 
 """
